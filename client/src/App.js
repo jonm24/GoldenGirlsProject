fcb28fb2c0087f161bc985305684276e3aa3d19c
Jonathan Michalak <themichalakfeed@gmail.com>
2022-01-03 23:18:35 -0600

    react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Golden Girls Client App</h1>
+      <div className="flex">
+        <button className="btn">Create</button>
+        <button className="btn">Read</button>
+        <button className="btn">Update</button>
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
