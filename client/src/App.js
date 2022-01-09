a0208e71da93ffb46e6d0b8023af0fd33638066b
Jonathan Michalak <themichalakfeed@gmail.com>
2022-01-08 20:53:10 -0600

    add modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import "./index.css";
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import bernie from "./bern.jpg";
 import { fakeEvents, fakeUsers } from "./fakeData";
 import { GlobalContext } from "./GlobalContext";
+import { Upcoming } from './components/Upcoming';
+import { Events } from './components/Events';
 
 export default function App() {
   const [user, setUser] = useState(fakeUsers[2])
+  const [allEvents, setAllEvents] = useState(fakeEvents)
   const [decidedList, setDecidedList] = useState(
     Object.entries(user['events'])
       .filter(value => value[1] === "Yes" || value[1] === "Maybe")
@@ -20,7 +23,7 @@ export default function App() {
           <input type="text" className="search" placeholder="Search"></input>
         </div>
         <div className="main-container">
-          <GlobalContext.Provider value={{ user, setUser, decidedList, setDecidedList }}>
+          <GlobalContext.Provider value={{ user, setUser, decidedList, setDecidedList, allEvents, setAllEvents }}>
             <Upcoming />
             <Events />
           </GlobalContext.Provider>
@@ -36,105 +39,6 @@ function User() {
       <div className="welcome">
         <img src={bernie} id="bernie" alt="bernie" />
         <h3 id="welcome-user">Welcome, Bernie</h3>
-      </div>
-    </div>
-  )
-}
-
-function Upcoming() {
-  const { decidedList } = useContext(GlobalContext)
-
-  return (
-    <div style={{ textAlign: "center", position: 'relative' }}>
-      <h2>Upcoming Events</h2>
-      <div className="upcoming-container">
-      {decidedList.map((event, index)=>(
-          <UpcomingCard key={index}  id={event[0]} status={event[1]}/>
-          ))
-        }
-      </div>
-
-      {/* covers cards getting cut off */}
-      <div className="covering"></div>
-      <div className="covering" style={{bottom: '580px'}}></div>
-    </div>
-  );
-}
-
-function UpcomingCard({ id, status }) {
-  let elem = fakeEvents.find(elem => elem.id === id)
-  let splitLocale = elem['location'].split('/')
-  let cleanLocale = `${splitLocale[0]}, ${splitLocale[1]}`
-
-  return (
-    <div className="card flex column" style={{backgroundColor: "#2e2e2e", color: "white"}}> 
-      <h1 className="eventName" style={{color: "white"}}>{elem.name}</h1>
-      <p style={{margin: '5px 0px 0px 0px', fontSize: '12px'}}>{elem["description"]}</p>
-      <div className="flex" style={{alignSelf: 'flex-end', width: '100%', alignItems: 'baseline', justifyContent: 'space-between'}}>
-        <h2 style={{color: "white"}}>{cleanLocale}</h2>
-        {
-          status === "Yes" ?
-            <h3 className="yes">✅</h3>
-          : status === "Maybe" ?
-            <h3 className="maybe">❓</h3>
-          :
-            <h3 className="no">❌</h3>
-        }
-      </div>
-    </div>
-  )
-}
-
-
-function Events() {
-  return (
-    <div style={{ textAlign: "center", position: 'relative' }}>
-      <h2>My Events</h2>
-      <button className="create-event"></button>
-      <div className="events-container">
-        {fakeEvents.map((elem, index)=>(
-          <Card key={index}  elem={elem} />
-          ))
-        }
-      </div>
-    </div>
-  );
-}
-function Card({ elem }) {
-  const { user, setUser, decidedList, setDecidedList } = useContext(GlobalContext)
-  let attending = user['events'][elem['id']]
-  let splitLocale = elem['location'].split('/')
-  let cleanLocale = `${splitLocale[0]}, ${splitLocale[1]}`
-
-  function setEvent(decision) {
-    let userEvents = user['events']
-    userEvents[elem['id']] = decision
-    setUser({...user, userEvents})
-    if (decision !== "No") {
-      setDecidedList([[elem.id, decision], ...decidedList])
-    }
-  }
-
-  return (
-    <div className="card flex column"> 
-      <h1 className="eventName">{elem.name}</h1>
-      <p style={{margin: '5px 0px 0px 0px', fontSize: '12px'}}>{elem["description"]}</p>
-      <div className="flex" style={{alignSelf: 'flex-end', width: '100%', alignItems: 'baseline', justifyContent: 'space-between'}}>
-        <h2>{cleanLocale}</h2>
-        {
-          !attending ? 
-            <div className="unsure flex">
-              <h3 onClick={() => setEvent("Yes")} className="yes">✅</h3> 
-              <h3 onClick={() => setEvent("Maybe")} style={{margin: '0px 12px'}} className="maybe">❓</h3>
-              <h3 onClick={() => setEvent("No")} className="no">❌</h3>
-            </div>
-          : attending === "Yes" ?
-            <h3 className="yes">✅</h3>
-          : attending === "Maybe" ?
-            <h3 className="maybe">❓</h3>
-          :
-            <h3 className="no">❌</h3>
-        }
       </div>
     </div>
   )
