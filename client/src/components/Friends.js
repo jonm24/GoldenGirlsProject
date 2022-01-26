68945486ea40b3a3b8ccc1036158f7e1dbc3f243
Jonathan Michalak <themichalakfeed@gmail.com>
2022-01-26 14:18:53 -0600

    user login

---
+++ client/src/components/Friends.js
@@ -0,0 +1,149 @@
+import { useState, useContext, useEffect, Fragment } from "react"
+import { GlobalContext } from "../GlobalContext"
+
+export function Friends() {
+  const { setFriendsPage, search, currentUser } = useContext(GlobalContext)
+  const [pending, setPending] = useState([])
+  const [allUsers, setAllUsers] = useState([])
+
+  function pendingDispatch(obj) {
+    if (pending?.length) {
+      setPending([...pending, obj])
+    } else {
+      setPending([obj])
+    }
+  }
+
+  useEffect(() => {
+    fetch(`https://q1weuz.deta.dev/friends/page/${currentUser.key}`)
+      .then(data => data.json())
+      .then(data => {
+        setPending(data.pending)
+        setAllUsers(data.users.value)
+        return
+      })
+  }, [currentUser])
+
+  return (
+    <div style={{textAlign: "center", position: "relative"}}>
+      <button onClick={() => setFriendsPage(false)} className="friends-back-btn">{`Back`}</button>
+      <div className="friends-container">
+        <h3>My Friends</h3>
+        {
+          !allUsers?.length ?
+            <h3>Fetching users and pending requests...</h3>
+          :  
+            <Fragment>
+            {currentUser.friends.map((elem, index) => (
+                <Friend
+                  key={index}
+                  name={elem}
+                  isFriend={true}
+                />
+              ))
+            }
+            <h3>Requests</h3>
+            {
+              pending?.length ? 
+              pending.map((elem, index) => (
+                <Friend 
+                  key={index}
+                  id={elem.key}
+                  acceptable={elem.to === currentUser.key}
+                  name={elem.to === currentUser.key ? elem.from : elem.to}
+                  pending={true}
+                />
+              ))
+              : 
+              <h3>No Pending Requests</h3>
+            }
+            <h3>All Users</h3>
+            {
+              allUsers.filter(user => {
+                if (
+                    (!search || (search && user.toLowerCase().startsWith(search.toLowerCase()))) 
+                    && user.toLowerCase() !== currentUser.key.toLowerCase() 
+                    && !pending?.find(elem => 
+                      (elem.to.toLowerCase() === user.toLowerCase() || elem.from.toLowerCase() === user.toLowerCase())
+                    )
+                    && !currentUser?.friends.find(elem => 
+                      elem.toLowerCase() === user.toLowerCase()
+                    )
+                  ) {
+                  return user
+                }
+                return false
+              }).map((user, index) => (
+                <Friend 
+                  key={index}
+                  name={user}
+                  pendingDispatch={pendingDispatch}
+                />
+              ))
+            }
+            </Fragment>
+        }
+      </div>
+    </div>
+  )
+}
+
+function Friend({ name, pending, acceptable, id, isFriend, pendingDispatch }) {
+  const { currentUser, setCurrentUser } = useContext(GlobalContext)
+  const [isLoading, setIsLoading] = useState(false)
+
+  function sendFriendReq() {
+    setIsLoading(true)
+    fetch(`https://q1weuz.deta.dev/friends/new`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "from": currentUser.key,
+        "to": name, 
+      })
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        if (data.result) {
+          pendingDispatch(data.result)
+          setIsLoading(false)
+          return
+        }
+        alert("Please try again")
+      })
+  }
+
+  function acceptFriendReq() {
+    setIsLoading(true)
+    fetch(`http://127.0.0.1:5000/friends/accept/${id}/${currentUser.key}/${currentUser.version}`, { method: "PUT"})
+      .then(data => data.json())
+      .then(data => { 
+        if (data.result) {
+          setCurrentUser(data.result)
+          setIsLoading(false)
+          return
+        }
+        alert("Please try again")
+      })
+  }
+
+  return (
+    <div className="friends-item-container">
+      <h3>{name}</h3>
+      {
+        isLoading ?
+          <h3>loading...</h3>
+        : pending && !acceptable ? 
+          <h3>Pending</h3>
+        : pending && acceptable ?
+          <button onClick={() => acceptFriendReq()}className="add-friend">Accept</button>
+        : !isFriend ?
+          <button onClick={() => sendFriendReq()} className="add-friend">Add Friend</button>
+        : 
+          null
+      }
+    </div>
+  )
+}
--- server/main.py
+++ server/main.py
@@ -3,7 +3,9 @@ from flask_cors import CORS
 from deta import Deta
 
 deta = Deta()
-db = deta.Base('simpleDB')
+global_db = deta.Base('global') # where global app data is stored
+users_db = deta.Base('users') # where user objects are stored
+friends_db = deta.Base('friends') # where friend requests are stored
 app = Flask(__name__)
 CORS(app)
 
@@ -12,35 +14,85 @@ CORS(app)
 def index():
     return jsonify(status="ok")
 
-# create and update. 
-# to create, just pass data (key generated)
-# to update, pass existing key with updated data
-@app.route("/text", methods=["PUT"])
-def update_text():
-    text = db.put(request.json)
-    return text if text else jsonify({"error": "Not found"}, 404)
+# login in user 
+# make a new object if one doesn't exist and return user
+# catch error if it does exist and return user
+@app.route("/users/login/<key>", methods=["GET"])
+def user_login(key):
+    try: 
+        user = users_db.insert({"events": [], "friends": [], "version": 0}, key)
+        global_db.update({"value": global_db.util.append(key)}, "all_users")
+        return jsonify(user=user)
+    except Exception as ex:
+        user = users_db.get(key)
+        return jsonify(error=f"{ex}", user=user)
 
-# get text object by it's key
-@app.route("/text/<key>", methods=["GET"])
-def get_text(key):
-    text = db.get(str(key))
-    return text if text else jsonify({"error": "Not found"}, 404)
+# make a new friend request
+@app.route("/friends/new", methods=["POST"])
+def friend_request():
+    res = friends_db.put(request.json)
+    return jsonify(result=res if res else False)
 
-# delete text object by it's key
-@app.route("/text/<key>", methods=["DELETE"])
-def delete_text(key):
-    db.delete(str(key))
-    return jsonify(deleted=str(key))
+# endpoint that gets hit when you go to friends page
+# get pending friend requests
+# get all users
+@app.route('/friends/page/<key>', methods=["GET"])
+def friends_page(key):
+    all_users = global_db.get("all_users")
+    res = friends_db.fetch([{"to?contains": key}, {"from?contains": key}])
+    return jsonify(
+        pending=res.items if res.items else None, 
+        users=all_users if all_users else None
+    )
 
-# get all text objects
-@app.route("/texts", methods=["GET"])
-def get_texts():
-    res = db.fetch()
-    all_items = res.items
+@app.route('/friends/accept/<key>/<to>/<version>', methods=["PUT"])
+def accept_friend(key, to, version):
+    req = dict(friends_db.get(str(key)))
+    receiver = dict(users_db.get(to))
+    if int(version) == receiver.get('version'):
+        users_db.update({ "friends": users_db.util.append(req.get('from')), "version": users_db.util.increment() }, to)
+        users_db.update({ "friends": users_db.util.append(to), "version": users_db.util.increment() }, req.get('from'))
+        friends_db.delete(str(key))
+        new_user = users_db.get(to) 
+        return jsonify(result=new_user)
+    else:
+        return jsonify(result=False)
 
-    # fetch until last is 'None'
-    while res.last:
-        res = db.fetch(last=res.last)
-        all_items += res.items
 
-    return jsonify(results=all_items)
+
+
+### BOILERPLATE CODE ###
+### ONLY FOR EXAMPLE ###
+
+# # create and update. 
+# # to create, just pass data (key generated)
+# # to update, pass existing key with updated data
+# @app.route("/text", methods=["PUT"])
+# def update_text():
+#     text = db.put(request.json)
+#     return text if text else jsonify({"error": "Not found"}, 404)
+
+# # get text object by it's key
+# @app.route("/text/<key>", methods=["GET"])
+# def get_text(key):
+#     text = db.get(str(key))
+#     return text if text else jsonify({"error": "Not found"}, 404)
+
+# # delete text object by it's key
+# @app.route("/text/<key>", methods=["DELETE"])
+# def delete_text(key):
+#     db.delete(str(key))
+#     return jsonify(deleted=str(key))
+
+# # get all text objects
+# @app.route("/texts", methods=["GET"])
+# def get_texts():
+#     res = db.fetch()
+#     all_items = res.items
+
+#     # fetch until last is 'None'
+#     while res.last:
+#         res = db.fetch(last=res.last)
+#         all_items += res.items
+
+#     return jsonify(results=all_items)
